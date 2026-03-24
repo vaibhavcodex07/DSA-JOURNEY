12d3846b2bc9d17da2ea6353e3c5da72b032dd87
Vaibhav <vaibhavbharti7082@gmail.com>
2026-03-24 14:10:37 +0530

    Searching in java...

---
+++ SEARCHING/desendingbinary.java
@@ -0,0 +1,24 @@
+public class desendingbinary {
+    public static void main(String[] args) {
+        int[] arr = {100,91,87,66,52,43,35,30,29,13,5};
+
+        int lo = 0;
+        int hi = arr.length - 1;
+        int target = 29;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            if (arr[mid] == target) {
+                System.out.println("Target found at index :-"+mid);
+                break;
+            }
+            else if (arr[mid] > target) {
+                lo = mid + 1;
+            }
+            else {
+                hi = mid - 1;
+            }
+        }
+    }
+}
